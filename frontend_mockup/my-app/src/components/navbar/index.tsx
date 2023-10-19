a797531018496994c52feeebb65e8e18c3def8d9
rajahwu <vmradford@gmail.com>
2023-10-19 15:57:58 -0400

    layout landing page

--- frontend_mockup/my-app/src/components/navbar/index.tsx
+++ frontend_mockup/my-app/src/components/navbar/index.tsx
@@ -2,19 +2,20 @@ import Logo from "../logo"
 
 export default function NavBar() {
     return (
-        <header className="flex space-x-auto">
-            <div className="container">
+        <header className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+
+            <div>
                 <Logo />
             </div>
 
-            <div className="container">
+            <div>
                 <div className="flex">
-                    <ul className="flex">
-                        <li>menu</li>
-                        <li>events</li>
-                        <li>location</li>
-                        <li>contact</li>
-                        <li>cart</li>
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">menu</li>
+                        <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">events</li>
+                        <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">location</li>
+                        <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">contact</li>
+                        <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">cart</li>
                     </ul>
                 </div>
 
