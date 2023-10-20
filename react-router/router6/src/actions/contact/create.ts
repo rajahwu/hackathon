5ceb07c15be0dcdf6e4659c8dc80816d225bcbe3
rajahwu <vmradford@gmail.com>
2023-10-19 21:44:02 -0400

    add active link styles

--- react-router/router6/src/actions/contact/create.ts
+++ react-router/router6/src/actions/contact/create.ts
@@ -1,9 +1,9 @@
+import { redirect } from "react-router";
 import { createContact } from "../../contacts";
 
 export async function action() {
     const contact = await createContact()
-    console.log(contact)
-    return { contact }
+    return redirect(`/contacts/${contact.id}/edit`)
   }
   
   
