7dff0c74fa7d9395183d014e8ddaf5f1f3820ea6
rajahwu <vmradford@gmail.com>
2023-10-20 00:48:48 -0400

    add menu item card

--- frontend_mockup/my-app/src/pages/landing/index.tsx
+++ frontend_mockup/my-app/src/pages/landing/index.tsx
@@ -3,12 +3,22 @@ import SiteNavBar from "../../components/navbar";
 import SplashPage from "../../components/splash";
 import MenuNavBar from "../../components/menu/navbar";
 
+import MenuItemCard from "../../components/menu/itemcard";
+
 export default function LandingPage() {
     return (
         <HomePageLayout>
             <SiteNavBar />
             <SplashPage />
             <MenuNavBar />
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                <MenuItemCard />
+                <MenuItemCard />
+                <MenuItemCard />
+                <MenuItemCard />
+                <MenuItemCard />
+                <MenuItemCard />
+            </div>
         </HomePageLayout>
     )
 } 
