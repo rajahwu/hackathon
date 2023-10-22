8284a2d9bc49a786db2833bfa34379b539d29bd5
rajahwu <vmradford@gmail.com>
2023-10-22 17:43:19 -0400

    add mdx

--- docs/hackathon-project-docs-bunx/scripts/create_sections.sh
+++ docs/hackathon-project-docs-bunx/scripts/create_sections.sh
@@ -26,12 +26,23 @@ sections=(
 # Create the sections directory if it doesn't exist
 mkdir -p src/pages/sections
 
-# Loop through section names and create corresponding .md files
+# Loop through section names and create or update corresponding .mdx files
 for section in "${sections[@]}"; do
-    # Generate the file name
-    filename=$(echo "$section" | tr ' ' '_' | tr -d ',')
-    # Create the .md file with the section name
-    touch "src/pages/sections/${filename}.md"
-    # Add a placeholder title to the .md file
-    echo "# ${section}" > "src/pages/sections/${filename}.md"
+    # Generate the file name in lowercase with .mdx extension
+    filename=$(echo "$section" | tr ' ' '_' | tr -d ',' | tr '[:upper:]' '[:lower:]').mdx
+    file_path="src/pages/sections/$filename"
+    
+    if [ -e "$file_path" ]; then
+        # Check if the existing file has an uppercase name or .md extension
+        if [[ "$file_path" != "src/pages/sections/$filename" ]]; then
+            # Rename the file to lowercase with .mdx extension
+            mv "$file_path" "src/pages/sections/$filename"
+            echo "Renamed $file_path to src/pages/sections/$filename."
+        else
+            echo "File '$file_path' already exists. Updating contents."
+        fi
+    fi
+
+    # Create or update the .mdx file with the section name
+    echo "# ${section}" > "src/pages/sections/$filename"
 done
