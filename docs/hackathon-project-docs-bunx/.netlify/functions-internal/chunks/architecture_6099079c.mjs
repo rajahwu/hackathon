985f0c6f4fa57e1483a17e94a9519a407b96fa81
rajahwu <vmradford@gmail.com>
2023-10-22 17:55:21 -0400

    update deployment config

---
+++ docs/hackathon-project-docs-bunx/.netlify/functions-internal/chunks/architecture_6099079c.mjs
@@ -0,0 +1,10 @@
+export { renderers } from '../renderers.mjs';
+import 'react';
+import 'react-dom/server';
+import './astro_3555f0b8.mjs';
+import 'html-escaper';
+import 'clsx';
+
+const page = () => import('./pages/architecture_c585c6b3.mjs');
+
+export { page };
---
+++ docs/hackathon-project-docs-bunx/.netlify/functions-internal/chunks/directory_structure_b06eabb7.mjs
@@ -0,0 +1,10 @@
+export { renderers } from '../renderers.mjs';
+import 'react';
+import 'react-dom/server';
+import './astro_3555f0b8.mjs';
+import 'html-escaper';
+import 'clsx';
+
+const page = () => import('./pages/directory_structure_2cc7cf4c.mjs');
+
+export { page };
